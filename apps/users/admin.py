d9d40afe95b241b81dc99e99279c02a6347dc2bf
dvuud <baltabaevdavud88@gmail.com>
2024-04-27 11:04:41 +0600

    First Commit

---
+++ apps/users/admin.py
@@ -0,0 +1,3 @@
+from django.contrib import admin
+# from .models import User, HistoryTransfer
+# Register your models here.
---
+++ apps/users/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.crypto import get_random_string
+from django.core.validators import RegexValidator 
+
+phone_regex = RegexValidator(regex=r'^\+996\d{9}$', message="Номер телефона необходимо ввести в формате: '+996xxxxxxxxx'.")
+
+class User(AbstractUser):
+    phone_number = models.CharField(validators=[phone_regex], max_length=15, verbose_name='Номер телефона')
+    age = models.IntegerField(default=0, verbose_name='Возраст')
+    wallet_address = models.CharField(max_length=12, unique=True,blank=True ,verbose_name='ID кошелька')
+
+    def save(self, *args, **kwargs):
+        if not self.wallet_address:
+            self.wallet_address = get_random_string(length=12)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return f'Пользователи'
+    
+    class Meta:
+        verbose_name = 'Пользователя'
+        verbose_name_plural = 'Пользователи'
+    
+class HistoryTransfer(models.Model):
+    from_user = models.ForeignKey(User, related_name='transfers_sent', on_delete=models.CASCADE)
+    to_user = models.ForeignKey(User, related_name='transfers_received', on_delete=models.CASCADE)
+    is_completed = models.BooleanField(default=False, verbose_name='Отправлено в')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Создан в')
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Сумма')
