c9260b4b7be4eb62a366a9a4c3e31757900d4de3
dvuud <baltabaevdavud88@gmail.com>
2025-01-09 19:51:35 +0600

    New commit

--- apps/users/permissions.py
+++ apps/users/permissions.py
@@ -4,3 +4,4 @@ from rest_framework import permissions
 class UserPermissions(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(obj.pk == request.pk)
+# jkdsjjsjsj
